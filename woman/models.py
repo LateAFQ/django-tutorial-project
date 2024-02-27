24a2ef595c4ae53943e0d28d90519763c8f4753b
LateAFQ <ssergeenko821@gmail.com>
2024-02-27 22:16:16 +0300

    adding a link between tables

--- woman/models.py
+++ woman/models.py
@@ -20,6 +20,7 @@ class Women (models.Model):
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
     slug = models.SlugField(max_length=255, db_index=True, unique=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     objects = models.Manager()
     published = PublishedModel()
@@ -29,3 +30,11 @@ class Women (models.Model):
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
