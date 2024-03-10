4bb6a47869041448400927a3e70e7c671c6a9db4
LateAFQ <ssergeenko821@gmail.com>
2024-03-10 18:53:33 +0300

    added: search_fields, list_filter

--- woman/admin.py
+++ woman/admin.py
@@ -11,6 +11,8 @@ class WomenAdmin(admin.ModelAdmin):
     list_editable = ('is_published', 'cat')
     list_per_page = 5
     actions = ['set_published', 'set_draft']
+    list_filter = ['cat__name', 'is_published']
+    search_fields = ['title__startswith', 'cat__name']
 
     @admin.display(description="Краткое описание", ordering='content')
     def brief_info(self, women: Women):
