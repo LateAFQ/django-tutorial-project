5f8d9602c75be383391d2b901b8a9b6f5cf5f27b
LateAFQ <ssergeenko821@gmail.com>
2024-03-10 17:34:14 +0300

    Adding actions and additional fields in admin fields

--- woman/admin.py
+++ woman/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Women, Category
 
 # Register your models here.
@@ -6,10 +6,29 @@ from .models import Women, Category
 
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'cat','is_published')
+    list_display = ('id', 'title', 'time_create', 'cat', 'is_published', 'brief_info')
     list_display_links = ('id', 'title')
     list_editable = ('is_published', 'cat')
     list_per_page = 5
+    actions = ['set_published', 'set_draft']
+
+    @admin.display(description="Краткое описание", ordering='content')
+    def brief_info(self, women: Women):
+        return f"Описание {len(women.content)} символов."
+
+    @admin.action(description="Опубликовать выбранные записи")
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.PUBLISHED)
+        if count == 1:
+            self.message_user(request, f"Опубликована {count} запись.")
+        self.message_user(request, f"Опубликовано {count} записи(ей).")
+
+    @admin.action(description="Снять с публикации выбранные записи")
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.DRAFT)
+        if count == 1:
+            self.message_user(request, f"{count} запись снята с публикации!", messages.WARNING)
+        self.message_user(request, f"{count} записи(ей) снята с публикации!", messages.WARNING)
 
 
 @admin.register(Category)
