04f9ee705571d71f1224635dfdc540ade9c39257
LateAFQ <ssergeenko821@gmail.com>
2024-03-15 20:09:43 +0300

    update under add_page

--- woman/forms.py
+++ woman/forms.py
@@ -3,8 +3,8 @@ from .models import Category
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(), required=False)
-    is_published = forms.BooleanField(required=False)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=255, label="Заголовок")
+    slug = forms.SlugField(max_length=255, label="URL")
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label="Контент")
+    is_published = forms.BooleanField(required=False, label="Статус")
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label="Категории", empty_label='Выберите категорию')
